d5201c06c9d7119a0e22279dab4410391a9b91d9
Venus <venus.izadi@gmail.com>
2016-12-04 12:52:12 +1100

    Adding state to the class

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,30 @@
-import React, { Component } from 'react';
+import React, {Component, PropTypes} from 'react';
 import './App.css';
 
-const App = () => {
-    return <h1>Hello</h1>
+class App extends Component {
+
+    constructor(){
+        super();
+        this.state = {myState: "initial state"};
+    };
+
+    update(e){
+      this.setState({myState: e.target.value});
+    };
+
+    render() {
+        return (
+            <div>
+                <input placeholder={this.state.myState} onChange={this.update.bind(this)}/>
+                <h2>{this.state.myState}</h2>
+            </div>);
+    }
+}
+;
+
+App.propTypes = {
+    text: PropTypes.string.isRequired,
 };
 
 export default App;
+
